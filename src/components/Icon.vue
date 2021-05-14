1260ccf708e7b5f25262f57af6e260d517b3c9c8
jonian <jonian@hardpixel.eu>
2021-05-14 12:53:06 +0300

    add icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <i
+    :class="iconClass"
+    class="uil" />
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iconClass() {
+      return `uil-${this.name}`
+    }
+  }
+}
+</script>
