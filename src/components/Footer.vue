547cef51ecf815a90cf28bd12907927c4b8cf18b
jonian <jonian@hardpixel.eu>
2021-05-14 23:09:09 +0300

    add credits in footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,21 @@
 <template>
   <footer class="border-t bg-white text-gray-darker dark:bg-black dark:border-gray-darkest">
-    <div class="flex items-center justify-between container py-2">
+    <div class="flex items-center justify-between container py-4">
+      <a
+        href="https://github.com/jonian/awmonitor"
+        target="_blank"
+        class="flex flex-nowrap items-center space-x-2">
+        <Icon name="github" class="text-gray-light transform scale-125" />
+        <span class="text-xs">View on GitHub</span>
+      </a>
+      <a
+        href="https://vuejs.org"
+        target="_blank"
+        class="flex flex-nowrap items-center space-x-2">
+        <span class="text-xs">Created with</span>
+        <Icon name="vuejs" class="text-gray-light transform scale-125" />
+        <span class="text-xs">Vue.js</span>
+      </a>
     </div>
   </footer>
 </template>
