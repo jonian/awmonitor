d841f6f85379917f4c068def43da938bd4d897ef
jonian <jonian@hardpixel.eu>
2021-05-14 23:30:58 +0300

    add send a tip component

---
+++ src/components/SupportCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center justify-between p-8 bg-primary text-white rounded-md shadow-sm">
+    <h2 class="text-2xl font-bold">Do you like this project?</h2>
+    <RoundedButton
+      icon="smile"
+      @click="onSendClick">
+      Send a tip
+    </RoundedButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SupportCard',
+  methods: {
+    onSendClick() {
+      this.$emit('send')
+    }
+  }
+}
+</script>
