4a76e45a2b2c94d300dda8e66f4ce15b676008a5
jonian <jonian@hardpixel.eu>
2021-05-15 21:29:55 +0300

    optimize button components

--- src/components/RoundedButton.vue
+++ src/components/RoundedButton.vue
@@ -3,7 +3,7 @@
     :style="{ borderColor: 'currentColor' }"
     :class="{ 'opacity-50': disabled, 'pointer-events-none': disabled }"
     class="rounded-full border px-6 py-2 outline-none focus:outline-none">
-    <span class="flex flex-nowrap items-baseline space-x-2">
+    <span class="flex flex-nowrap items-baseline justify-center space-x-2">
       <Icon
         v-if="icon"
         :name="icon" />
--- src/components/SimpleButton.vue
+++ src/components/SimpleButton.vue
@@ -2,7 +2,7 @@
   <button
     :class="{ 'opacity-50': disabled, 'pointer-events-none': disabled }"
     class="rounded-md px-4 py-2 outline-none focus:outline-none">
-    <span class="flex flex-nowrap items-baseline space-x-2">
+    <span class="flex flex-nowrap items-baseline justify-center space-x-2">
       <Icon
         v-if="icon"
         :name="icon" />
