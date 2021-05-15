45c6228ec05ec89587b9d4b1458c34dd54a061ed
jonian <jonian@hardpixel.eu>
2021-05-15 04:06:39 +0300

    add home page components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,51 @@
 <template>
-  <span>Home</span>
+  <div class="flex items-center justify-between mb-6">
+    <div class="flex items-center space-x-4">
+      <h2 class="font-bold text-xl">Accounts</h2>
+      <Icon
+        name="plus-circle"
+        class="text-primary transform scale-150 cursor-pointer"
+        @click="onAddAccount" />
+    </div>
+    <span class="text-sm text-gray-light">
+      Last update: {{ updatedAt }}
+    </span>
+  </div>
+  <div class="flex flex-col space-y-6">
+    <template v-for="account in $app.accounts">
+      <AccountCard
+        v-if="!account.loading && !account.error"
+        :key="account.name"
+        :account="account" />
+    </template>
+    <SupportCard />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  inject: ['$app'],
+  data() {
+    return {
+      newAccount: null
+    }
+  },
+  computed: {
+    updatedAt() {
+      const date = this.$app.updatedAt
+      const time = date.toLocaleTimeString([], { hour12: false })
+
+      return `${date.toDateString()} ${time}`
+    }
+  },
+  methods: {
+    onAddAccount() {
+      if (this.newAccount) {
+        this.$app.addAccount(this.newAccount)
+        this.newAccount = null
+      }
+    }
+  }
 }
 </script>
