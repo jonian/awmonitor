72cd4e5a8bf6b85e347add07b3c26ee9a3e48b48
jonian <jonian@hardpixel.eu>
2021-08-21 16:20:07 +0300

    add aria-label to buttons

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     :class="{ 'opacity-50': disabled, 'pointer-events-none': disabled }"
+    :aria-label="text"
     class="p-2 outline-none focus:outline-none">
     <span class="flex flex-nowrap items-baseline justify-center space-x-2">
       <Icon
